79d3038013a57635381328ad1698a9be34413b3d
MarvinThomaz <marvinthomaz@gmail.com>
2016-08-25 09:33:34 -0300

    Add files via upload

--- js/pontuacao.js
+++ js/pontuacao.js
@@ -1,18 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('PontuacaoController', function($scope, $rootScope, $ionicPlatform) { 
-    
-    $scope.voltar = back;
-    $scope.pontuacao = [
-            { text: 'Fácil: ' + (localStorage.getItem("maiorPontuacaoFacil") == null ? 0 : localStorage.getItem("maiorPontuacaoFacil")) }, 
-            { text: 'Normal: ' + (localStorage.getItem("maiorPontuacaoNormal") == null ? 0 : localStorage.getItem("maiorPontuacaoNormal")) }, 
-            { text: 'Difícil: ' + (localStorage.getItem("maiorPontuacaoDificil") == null ? 0 : localStorage.getItem("maiorPontuacaoDificil")) }, 
-            { text: 'Insano: ' + (localStorage.getItem("maiorPontuacaoInsano") == null ? 0 : localStorage.getItem("maiorPontuacaoInsano")) }
+    .controller('PontuacaoController', function ($scope, $rootScope, $ionicPlatform) {
+
+        $scope.voltar = back;
+        $scope.pontuacao = [
+            { modo: 'Fácil', pontuacao: (localStorage.getItem("maiorPontuacaoFacil") == null ? 0 : localStorage.getItem("maiorPontuacaoFacil")) },
+            { modo: 'Normal', pontuacao: (localStorage.getItem("maiorPontuacaoNormal") == null ? 0 : localStorage.getItem("maiorPontuacaoNormal")) },
+            { modo: 'Difícil', pontuacao: (localStorage.getItem("maiorPontuacaoDificil") == null ? 0 : localStorage.getItem("maiorPontuacaoDificil")) },
+            { modo: 'Insano', pontuacao: (localStorage.getItem("maiorPontuacaoInsano") == null ? 0 : localStorage.getItem("maiorPontuacaoInsano")) }
         ];
 
-    voltar(back, $scope, $rootScope, $ionicPlatform);
-});
+        voltar(back, $scope, $rootScope, $ionicPlatform);
+    });
 
-function back(){
+function back() {
     window.location.href = "index.html";
 }
